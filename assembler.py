90a71158a0c13548d30b219c9c3222495e9e1feb
NinjaJc01 <james@cookiehome.co.uk>
2019-01-14 22:15:19 +0000

    Formatting of assembler improved

--- assembler.py
+++ assembler.py
@@ -22,6 +22,7 @@ INSTRUCTIONS = {
     "cmp": 13,
     "set": 14
 }
+
 symbol_table = {}
 PROGRAM_FILE = open("sample program.txt")
 PROGRAM = PROGRAM_FILE.readlines()
@@ -29,29 +30,28 @@ PROGRAM_FILE.close()
 address_counter = 0
 delabelled = ""
 for line in PROGRAM:
-    ##Split at spaces
+    # Split at spaces
     line = line.rstrip()
-    line=line.split(" ")
-    ##Check if first symbol is an instruction
+    line = line.split(" ")
+    # Check if first symbol is an instruction
     if line[0].lower() in INSTRUCTIONS:
         delabelled += " ".join(line)+"\n"
     else:
-        ##Add to symbol table if declaration
+        # Add to symbol table if declaration
         symbol_table[line[0]] = address_counter
         line = " ".join(line)
         key = line.split(" ")[0]
-        ##Replace the key with "" to remove it
-        line = re.sub(r"\b"+key+r"\b","",line)
+        # Replace the key with "" to remove it
+        line = re.sub(r"\b"+key+r"\b", "", line)
         line = line.lstrip()
         delabelled += line+"\n"
-    address_counter += 1 ##Used for symbol table
+    address_counter += 1  # Used for symbol table
 a = "".join(PROGRAM)
 print(symbol_table)
 to_assemble = ""
 for line in delabelled.split("\n"):
     for key, value in symbol_table.items():
-        line = re.sub(r"\b"+key+r"\b",str(value),line)
+        line = re.sub(r"\b"+key+r"\b", str(value), line)
     print(line)
     to_assemble += line+"\n"
-print("Final:\n",to_assemble,sep="")
-
+print("Final:\n", to_assemble, sep="")
