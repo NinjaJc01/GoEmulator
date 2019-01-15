607f103636d670a32a181f7e4fa224d864050f75
jc74537 <jcook.74537@farnborough.ac.uk>
2019-01-15 12:01:35 +0000

    Created working assembler, working emulator

--- assembler.py
+++ assembler.py
@@ -1,3 +1,4 @@
+"""No, pylint. This isn't a module, it's a script. So here's your docstring"""
 import re
 INSTRUCTIONS = {
     "hlt": 0,
@@ -23,35 +24,81 @@ INSTRUCTIONS = {
     "set": 14
 }
 
-symbol_table = {}
-PROGRAM_FILE = open("sample program.txt")
+
+def _twos_complement(number):
+    number = int(number)
+    if number < 0:
+        number *= -1
+        number = ("{0:<b}".format(number).rjust(16, '0'))
+        number_2 = ""
+        for bit in number:  # Flip the bits
+            if int(bit):
+                number_2 += "0"
+            else:
+                number_2 += "1"
+        # Convert to int as otherwise I'd have to do binary addition
+        int_value = int(number_2, 2)
+        int_value += 1  # add one
+        number = ("{0:<b}".format(int_value).rjust(
+            16, '0'))  # convert back to binary
+        return number
+    return "{0:b}".format(number).rjust(16, '0')
+
+
+SYMBOL_TABLE = {}
+PROGRAM_FILE = open("sample program.txt","r")
 PROGRAM = PROGRAM_FILE.readlines()
 PROGRAM_FILE.close()
-address_counter = 0
-delabelled = ""
+ADDRESS_COUNTER = 0
+DELABELLED = ""
 for line in PROGRAM:
     # Split at spaces
     line = line.rstrip()
     line = line.split(" ")
     # Check if first symbol is an instruction
     if line[0].lower() in INSTRUCTIONS:
-        delabelled += " ".join(line)+"\n"
+        DELABELLED += " ".join(line)+"\n"
     else:
         # Add to symbol table if declaration
-        symbol_table[line[0]] = address_counter
+        SYMBOL_TABLE[line[0]] = ADDRESS_COUNTER
         line = " ".join(line)
         key = line.split(" ")[0]
         # Replace the key with "" to remove it
         line = re.sub(r"\b"+key+r"\b", "", line)
         line = line.lstrip()
-        delabelled += line+"\n"
-    address_counter += 1  # Used for symbol table
-a = "".join(PROGRAM)
-print("Symbol Table:",symbol_table)
-to_assemble = ""
-for line in delabelled.split("\n"):
-    for key, value in symbol_table.items():
+        DELABELLED += line+"\n"
+    ADDRESS_COUNTER += 1  # Used for symbol table
+
+print("Symbol Table:", SYMBOL_TABLE)
+TO_ASSEMBLE = ""
+
+for line in DELABELLED.split("\n"):
+    for key, value in SYMBOL_TABLE.items():
         line = re.sub(r"\b"+key+r"\b", str(value), line)
     print(repr(line))
-    if line != '': to_assemble += line+"\n"
-print("Final:\n", to_assemble, sep="", end='')
+    if line != '':
+        TO_ASSEMBLE += line+"\n"
+
+print("Final:\n", TO_ASSEMBLE, sep="", end='')
+print()
+OUT_PROGRAM = ""
+for line in TO_ASSEMBLE.split("\n"):
+    if len(line.split(" ")) > 1:
+        items = line.split(" ")
+        ##print("Items: ",items)
+        operator = items[0]
+        operand = items[1]
+        if operand.startswith("/"):
+            operand = 0
+    else:
+        if len(line) > 2:
+            operator = line
+            operand = 0
+        else:
+            break
+    operator = INSTRUCTIONS[operator.lower()]
+    print(operator, operand)
+    OUT_PROGRAM += (_twos_complement(operator)+_twos_complement(operand)+"\n")
+OUT_FILE = open("machine code.txt","w")
+print(OUT_PROGRAM, file=OUT_FILE, end="")
+OUT_FILE.close()
--- main.go
+++ main.go
@@ -39,6 +39,7 @@ var (
 		13: cmp,
 		14: set,
 	}
+	program [memAmount]string
 )
 
 //Higher level logic
@@ -71,8 +72,13 @@ func split(x int32) (int16, int16) { //splits into operand and operator, -32768 
 	//fmt.Printf("%016b %016b\n", highBits, lowBits)
 	return int16(highBits), lowBits
 }
-
-func power2(y int32) (a int32) { //technically, base will always be 2, so I could write this better.
+func twosComplement(binary string) int16 {
+	total := -32768*int(binary[0])
+	toAdd, _ := strconv.ParseInt(binary[1:len(binary)],2,16)
+	total += int(toAdd)
+	return int16(total)
+}
+func power2(y int32) (a int32) {
 	a = int32(1)
 	for i := int32(0); i < y; i++ {
 		a *= 2
@@ -82,25 +88,41 @@ func power2(y int32) (a int32) { //technically, base will always be 2, so I coul
 
 func main() {
 	fmt.Printf("------ Prepared %06v bytes ------\n", memAmount*4)
-	//Arbitrary code goes here
-	set(196621) //STA 13
-	//fmt.Print("Splitty: ")
-	//fmt.Println(split(196621))
-	sta(0)
-	set(196623) //STA 15
-	sta(1)
-	set(-5)
-	sta(12)
-	//hlt(0)
-	bra(3)
-	//dumpMem(false)
-	set(-7)
-	bra(0)
-	//fmt.Println(decode(0))
+	program[0] = "00000000000010010000000000000000"
+	program[1] = "00000000000010000000000000000000"
+	program[2] = "00000000000001000000000000000110"
+	program[3] = "00000000000000100000000000000111"
+	program[4] = "00000000000010000000000000000000"
+	program[5] = "00000000000001100000000000000010"
+	program[6] = "00000000000000000000000000000000"
+	program[7] = "00000000000000000000000000000001"
+
+	for index,value := range(program){
+		if value != "" {
+			memory[index] = (int32(twosComplement(value[0:16]))*65536)+(int32(twosComplement(value[16:32])))
+		} else {
+			break
+		}
+	}
+	dumpMem(false)
+	// //Arbitrary code goes here
+	// set(196621) //STA 13
+	// //fmt.Print("Splitty: ")
+	// //fmt.Println(split(196621))
+	// sta(0)
+	// set(196623) //STA 15
+	// sta(1)
+	// set(-5)
+	// sta(12)
+	// //hlt(0)
+	// bra(3)
+	// //dumpMem(false)
+	// set(-7)
+	// bra(0)
+	// //fmt.Println(decode(0))
 	fmt.Println("-----------------------------------")
 	//fmt.Println("started",programcounter)
 	for programcounter >= 0 {
-		fmt.Println("a")
 		data := fetch()
 		if debug {
 			fmt.Println("data:", data, "pc: ", programcounter, "acc: ", acc)
@@ -131,7 +153,7 @@ func decode(data int32) (func(int32), int32) {
 		fmt.Println("Unknown instruction!", opIndex)
 		return hlt, 0
 	}
-	fmt.Println(opIndex, " : ", operand)
+	//fmt.Println(opIndex, " : ", operand)
 	return operator, int32(operand)
 }
 
